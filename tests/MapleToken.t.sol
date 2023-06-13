94d20718563815f525c4221104184122189a8c17
Vedran Bidin <vedran@bidin.me>
2023-06-13 21:25:15 +0300

    fix: finish up setting up repo

---
+++ tests/MapleToken.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.18;
+
+import { TestBase } from "./utils/TestBase.sol";
+
+// TODO: Potentially split up into multiple test files.
+contract MapleTokenTests is TestBase {
+
+}
