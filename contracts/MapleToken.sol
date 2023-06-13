c4b28673db80e3c2e6de837f31f9d2f53c6ba426
Vedran Bidin <vedran@bidin.me>
2023-06-13 21:43:16 +0300

    fix: update inheritance

--- contracts/MapleToken.sol
+++ contracts/MapleToken.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
-contract MapleToken is ERC20, NonTransparentProxy { }
+import { ERC20 }                 from "../modules/erc20/contracts/ERC20.sol";
+import { NonTransparentProxied } from "../modules/non-transparent-proxy/contracts/NonTransparentProxied.sol";
+
+// TODO: Overwrite ERC20 functionality.
+contract MapleToken is ERC20, NonTransparentProxied { }
