367ceaa0654df5880db0a2ab8bb5f3ad85fd122b
Vedran Bidin <vedran@bidin.me>
2023-06-13 21:33:54 +0300

    fix: enable scripts

--- contracts/MapleToken.sol
+++ contracts/MapleToken.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
-import { TestBase } from "./utils/TestBase.sol";
-
-contract MapleTokenTests is TestBase {
+contract MapleToken is ERC20, NonTransparentProxy { }
