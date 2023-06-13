6aaeb2cf05dba24432796be49d722aa021d98cf5
Vedran Bidin <vedran@bidin.me>
2023-06-13 21:50:15 +0300

    fix: add interfaces

--- contracts/MapleToken.sol
+++ contracts/MapleToken.sol
@@ -4,5 +4,9 @@ pragma solidity 0.8.18;
 import { ERC20 }                 from "../modules/erc20/contracts/ERC20.sol";
 import { NonTransparentProxied } from "../modules/non-transparent-proxy/contracts/NonTransparentProxied.sol";
 
-// TODO: Overwrite ERC20 functionality.
-contract MapleToken is ERC20, NonTransparentProxied { }
+contract MapleToken is IMapleToken, ERC20, NonTransparentProxied {
+
+    // TODO: Is this needed?
+    address public override globals;
+
+}
