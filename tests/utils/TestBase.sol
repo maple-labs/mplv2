102d23e071070b1af7252cb74b3cc1072eefb041
Joao Gabriel Carvalho <jg@jgcarv.com>
2023-06-15 15:02:45 -0300

    chore: Repo setup (SC-12654) (#5)
    * feat: add modules and update toml
    
    * fix: finish up setting up repo
    
    * fix: remove vscode settings
    
    * fix: enable scripts
    
    * fix: add note to update ci
    
    * fix: update inheritance
    
    * fix: add interfaces
    
    * fix: remove old submodules
    
    * fix: add new ssh submodule links
    
    ---------
    
    Co-authored-by: Vedran Bidin <vedran@bidin.me>

---
+++ tests/utils/TestBase.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.18;
+
+import { Test } from "../../modules/forge-std/src/Test.sol";
+
+// TODO: Check if this can be removed and `Test` inherited directly.
+contract TestBase is Test {
+
+    bytes internal constant assertionError      = abi.encodeWithSignature("Panic(uint256)", 0x01);
+    bytes internal constant arithmeticError     = abi.encodeWithSignature("Panic(uint256)", 0x11);
+    bytes internal constant divisionError       = abi.encodeWithSignature("Panic(uint256)", 0x12);
+    bytes internal constant enumConversionError = abi.encodeWithSignature("Panic(uint256)", 0x21);
+    bytes internal constant encodeStorageError  = abi.encodeWithSignature("Panic(uint256)", 0x22);
+    bytes internal constant popError            = abi.encodeWithSignature("Panic(uint256)", 0x31);
+    bytes internal constant indexOOBError       = abi.encodeWithSignature("Panic(uint256)", 0x32);
+    bytes internal constant memOverflowError    = abi.encodeWithSignature("Panic(uint256)", 0x41);
+    bytes internal constant zeroVarError        = abi.encodeWithSignature("Panic(uint256)", 0x51);
+
+}
