49a5807a96dc3689b006ccb451dcc6e357f340d5
JG Carvalho <jg@jgcarv.com>
2023-06-19 17:22:35 -0300

    feat: Add emergency module

--- contracts/interfaces/Interfaces.sol
+++ contracts/interfaces/Interfaces.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
+interface IERC20Like {
+
+    function mint(address _to, uint256 _value) external;
+
+    function burn(address _from, uint256 _value) external;
+
+}
+
 interface IGlobalsLike {
 
     function governor() external view returns (address governor);
