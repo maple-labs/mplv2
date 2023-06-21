f92994df054f7632bdeb49e9bc1897a080687e0c
Joao Gabriel Carvalho <jg@jgcarv.com>
2023-06-21 11:51:40 -0300

    feat: Add Emergency Module  (#7)
    * feat: Add emergency module
    
    * fix: PR comments

--- contracts/interfaces/Interfaces.sol
+++ contracts/interfaces/Interfaces.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
+interface IERC20Like {
+
+    function mint(address to, uint256 value) external;
+
+    function burn(address from, uint256 value) external;
+
+}
+
 interface IGlobalsLike {
 
     function governor() external view returns (address governor);
--- tests/utils/Mocks.sol
+++ tests/utils/Mocks.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
+import { Test } from "../../modules/forge-std/src/Test.sol";
+
+contract Spied is Test {
+
+    bool internal assertCalls;
+    bool internal captureCall;
+
+    uint256 callCount;
+
+    bytes[] internal calls;
+
+    modifier spied() {
+        if (captureCall) {
+            calls.push(msg.data);
+            captureCall = false;
+        } else {
+            if (assertCalls) {
+                assertEq(msg.data, calls[callCount++], "Unexpected call spied");
+            }
+
+            _;
+        }
+    }
+
+    function __expectCall() public {
+        assertCalls = true;
+        captureCall = true;
+    }
+
+}
+
 contract MockGlobals {
     
     address public governor;
@@ -12,6 +43,18 @@ contract MockGlobals {
 
     function __setMapleTreasury(address mapleTreasury_) external {
         mapleTreasury = mapleTreasury_;
+    }
+
+}
+
+contract MockToken is Spied {
+
+    function mint(address to, uint256 amount) external spied {
+        // do nothing
+    }
+
+    function burn(address from, uint256 amount) external spied {
+        // do nothing
     }
 
 }
