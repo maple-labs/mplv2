b572fe55fd2bae20d66a4a19625612ccb870440d
JG Carvalho <jg@jgcarv.com>
2023-06-21 13:55:27 -0300

    test: get accrued amount test

--- tests/utils/Harnesses.sol
+++ tests/utils/Harnesses.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
+
+import { InflationModule } from "../../contracts/InflationModule.sol";
+
+contract InflationModuleHarness is InflationModule {
+
+    constructor(address _token, address _globals, uint128 rate_) InflationModule(_token, _globals, rate_) {}
+
+    function __getAccruedAmount(uint256 timeElapsed_, uint256 supply_, uint256 rate_) external pure returns (uint256) {
+        return _getAccruedAmount(timeElapsed_, supply_, rate_);
+    }
+}
