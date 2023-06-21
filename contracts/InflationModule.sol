99c34f709bf355bb9f33bd7aaafd8274932ea053
JG Carvalho <jg@jgcarv.com>
2023-06-21 16:46:15 -0300

    feat: basic math  tests

--- contracts/InflationModule.sol
+++ contracts/InflationModule.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.18;
 
+import { console2 as console } from "../modules/forge-std/src/Test.sol";
+
 import { IERC20Like, IGlobalsLike } from "./interfaces/Interfaces.sol";
 
 contract InflationModule {
@@ -25,72 +27,87 @@ contract InflationModule {
     }
 
     function setRate(uint128 rate_) external {
+        // Timelock?
         require(msg.sender == IGlobalsLike(globals).governor(), "IM:SR:NOT_GOVERNOR");
 
         // do a withdraw, then save the new rate.
-        withdraw();
+        claim();
 
         rate = rate_;
     }
 
     function start() external {
-        periodStart = uint40(block.timestamp);
-        lastUpdated  = uint40(block.timestamp);
+        require(msg.sender == IGlobalsLike(globals).governor(), "IM:S:NOT_GOVERNOR");
 
-        supply = 11_000_000e18; // Starts with 11M tokens 
+        periodStart = uint40(block.timestamp);
+        lastUpdated = uint40(block.timestamp);
+        supply      = IERC20Like(token).totalSupply();
     }
 
     // Withdraw tokens from inflation rate
-    function withdraw() public {
-        require(periodStart != 0, "IM:W:NOT_STARTED");
+    function claim() public {
+        require(periodStart != 0, "IM:C:NOT_STARTED");
 
-        uint256 periodEnd_   = periodStart + PERIOD;
-        uint256 lastUpdated_ = lastUpdated;    
-        uint256 supply_      = supply; 
-        uint256 rate_        = rate;
-
-        uint256 amount = 0;
-        // It's past the timestamp when the principal should compound.
-        if (block.timestamp > periodEnd_) {
-            // Accrue last period's amount;
-            amount       = _getAccruedAmount(periodEnd_ - lastUpdated_, supply_, rate_);
-            lastUpdated_ = uint40(periodEnd_);
-
-            // Do a snapshot of the current supply and add what will be minted for the period.
-            supply_ = IERC20Like(token).totalSupply() + amount;
-
-            uint256 fullPeriods_ = (block.timestamp - lastUpdated_) / PERIOD;
-            uint256 period_      = fullPeriods_; 
-
-            // There is a more optimized version of this, using the compound interest formula, but realistically this code should never
-            // run, therefore a simpler version is used, to avoid using a scaled exponentiation function.
-            while (period_ > 0) {
-                uint256 periodAmount = _getAccruedAmount(PERIOD, supply_, rate_);
-                amount  += periodAmount;
-                supply_ += periodAmount;
-                period_--;
-            }
-            
-            lastUpdated_ = uint40(periodEnd_ + (fullPeriods_ * PERIOD)); 
-
-            periodStart = uint40(periodStart + (fullPeriods_ * PERIOD));
-            supply      = supply_;
-        }
-        
-        // Accrue the amount for the current period
-        amount += _getAccruedAmount(block.timestamp - lastUpdated_, supply_, rate_);
+        (uint256 amount, uint256 newSupply, uint256 newPeriodStart) = _dueTokensAt(block.timestamp);
 
         lastUpdated = uint40(block.timestamp);
+        periodStart = uint40(newPeriodStart);
+        supply      = newSupply;
 
         // Mint
         IERC20Like(token).mint(IGlobalsLike(globals).mapleTreasury(), amount);
     }
 
-    function _dueTokensFor() internal view returns (uint256 amount_) {
+    function _dueTokensAt(uint256 timestamp) internal view returns (uint256 amount_, uint256 newSupply_, uint256 newPeriodStart_) {
+        // Save variables to stack
+        newSupply_      = supply;
+        newPeriodStart_ = periodStart;
 
+        if (timestamp < lastUpdated) return (amount_, newSupply_, newPeriodStart_);
+        
+        uint256 rate_        = rate;
+        uint256 periodEnd_   = periodStart + PERIOD;
+        uint256 lastUpdated_ = lastUpdated;
+
+        // If it's past the period end, there's a need to compound the supply
+        if (timestamp > periodEnd_) {
+            // First, get the amount from lastUpdated to when the period ended.
+            amount_      = _interestFor(periodEnd_ - lastUpdated_, newSupply_, rate_);
+            lastUpdated_ = periodEnd_;
+
+            // Since at least full period has passed, the new supply is snapshotted and compounded. 
+            // Won't be precisely at the end of period, so there will be some amount of time where the supply is not updated, but that's fine. 
+            //  Adding `amount_` because tokens haven't been minted yet.
+            newSupply_ = IERC20Like(token).totalSupply() + amount_;
+
+            // Get the amounts of full periods that have passed. On most situations, this will be 0.
+            // There's no way to snapshot the supply at the end of each period, so the last known supply is used.
+            uint256 fullPeriods_ = (timestamp - lastUpdated_) / PERIOD;
+            uint256 period_      = fullPeriods_; 
+
+            // There is a more optimized version of this, using the compound interest formula, but realistically this code should never
+            // run, therefore a simpler version is used, to avoid using a scaled exponentiation function.
+            while (period_ > 0) {
+                // For every full period, the full interest is added to the amount, and the supply is compounded.
+                uint256 periodAmount = _interestFor(PERIOD, newSupply_, rate_);
+
+                amount_    += periodAmount;
+                newSupply_ += periodAmount;
+
+                period_--;
+            }
+
+            // Update the lastUpdated_ and newPeriodStart_ variables.
+            newPeriodStart_ = uint40(periodStart + ((fullPeriods_ + 1) * PERIOD));
+            lastUpdated_    = newPeriodStart_;
+        }
+
+        // This will handle both the case where the lastUpdates is within the same period and that the interval from 
+        // the new periodStart to the timestamp.
+        amount_ += _interestFor(timestamp - lastUpdated_, newSupply_, rate_);
     }
 
-    function _getAccruedAmount(uint256 interval_, uint256 supply_, uint256 rate_) internal pure returns (uint256 amount) {
+    function _interestFor(uint256 interval_, uint256 supply_, uint256 rate_) internal pure returns (uint256 amount) {
         amount = (supply_ * rate_ * interval_ * SCALE) / (PERIOD * SCALE * HUNDRED_PERCENT);
     }
 
--- tests/InflationModule.t.sol
+++ tests/InflationModule.t.sol
@@ -18,7 +18,7 @@ contract InflationModuleTestBase is TestBase {
     MockToken              token;
     InflationModuleHarness inflationModule;
 
-    function setUp() external {
+    function setUp() public virtual {
         globals = new MockGlobals();
         globals.__setGovernor(governor);
         globals.__setMapleTreasury(treasury);
@@ -31,12 +31,7 @@ contract InflationModuleTestBase is TestBase {
 
 contract GetAccruedAmountTests is InflationModuleTestBase {
 
-    function _assertAmount(uint256 supply, uint256 rate, uint256 interval, uint256 expected) internal {
-        uint256 actual = inflationModule.__getAccruedAmount(interval, supply, rate);
-        assertEq(actual, expected);
-    }
-
-    function test_getAccruedAmount_fixtures() external {
+    function test_interestFor_fixtures() external {
         _assertAmount(0, 0.05e6, 365 days, 0);
 
         _assertAmount(10_000_000e6, 0.05e6, 365 days * 4, 2_000_000e6);
@@ -49,4 +44,69 @@ contract GetAccruedAmountTests is InflationModuleTestBase {
         _assertAmount(1_234_567e6, 0.05e6, 365 days / 6, 10_288.058333e6);
     }
 
+    function _assertAmount(uint256 supply, uint256 rate, uint256 interval, uint256 expected) internal {
+        uint256 actual = inflationModule.__interestFor(interval, supply, rate);
+        assertEq(actual, expected);
+    }
+
+}
+
+contract DueTokensAtTests is InflationModuleTestBase {
+
+    uint256 year = 365 days;
+
+    uint256 start;
+    uint256 currentSupply = 10_000_000e18;
+
+    function setUp() public override {
+        super.setUp();
+
+        token.__setTotalSupply(currentSupply);
+
+        vm.prank(governor);
+        inflationModule.start();
+
+        start = block.timestamp;
+    }
+
+    function test_dueTokensAt_fixtures() external {
+
+        _assertDueTokensAt({
+            timestamp:            start + year / 2,  // 6 months
+            expectedAmount:       250_000e18, 
+            expectedSupply:       currentSupply, 
+            expectedPeriodStart:  start
+        });
+
+        _assertDueTokensAt({
+            timestamp:            start + year,  // Full Period
+            expectedAmount:       500_000e18, 
+            expectedSupply:       currentSupply, 
+            expectedPeriodStart:  start
+        });
+
+        _assertDueTokensAt({
+            timestamp:            start + year + year / 2,  // 1.5 years
+            expectedAmount:       500_000e18 + 262_500e18, 
+            expectedSupply:       currentSupply + 500_000e18, 
+            expectedPeriodStart:  start + year
+        });
+
+        _assertDueTokensAt({
+            timestamp:            start + (3 * year),                       // 3 years from now
+            expectedAmount:       500_000e18 + 525_000e18 + 551_250e18,     // 500k from 1st year + 525k from 2nd year + 551.250k from 3rd
+            expectedSupply:       currentSupply + 500_000e18 + 525_000e18 + 551_250e18, 
+            expectedPeriodStart:  start + (3 * year) 
+        });
+
+    }
+
+    function _assertDueTokensAt(uint256 timestamp, uint256 expectedAmount, uint256 expectedSupply, uint256 expectedPeriodStart) internal {
+        ( uint256 amount, uint256 supply, uint256 periodStart ) = inflationModule.__dueTokensAt(timestamp);
+
+        assertEq(amount,      expectedAmount);
+        assertEq(supply,      expectedSupply);
+        assertEq(periodStart, expectedPeriodStart);
+    }
+    
 }
