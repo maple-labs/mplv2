6b62b363aaf7d908ccbde8b355123cd0908e8af4
JG Carvalho <jg@jgcarv.com>
2023-06-26 11:48:50 -0300

    feat: add integration tests for emergency module

--- tests/utils/Interfaces.sol
+++ tests/utils/Interfaces.sol
@@ -3,16 +3,6 @@ pragma solidity 0.8.18;
 
 interface IGlobalsLike {
 
-    function scheduledCalls(
-        address caller_,
-        address contract_,
-        bytes32 functionId_
-    ) external view returns (uint256 timestamp, bytes32 dataHash);
-
-    function timelockParametersOf(address contract_, bytes32 functionId_) external view returns (uint128 delay, uint128 duration);
-
-    function setTimelockWindow(address contract_, bytes32 functionId_, uint128 delay_, uint128 duration_) external;
-
     function isValidScheduledCall(
         address          caller_,
         address          contract_,
@@ -20,8 +10,21 @@ interface IGlobalsLike {
         bytes   calldata callData_
     ) external view returns (bool isValid_);
 
+    function mapleTreasury() external view returns (address mapleTreasury_);
+
+    function scheduledCalls(
+        address caller_,
+        address contract_,
+        bytes32 functionId_
+    ) external view returns (uint256 timestamp, bytes32 dataHash);
+
+    function setMapleTreasury(address mapleTreasury_) external;
+
+    function setTimelockWindow(address contract_, bytes32 functionId_, uint128 delay_, uint128 duration_) external;
 
     function scheduleCall(address contract_, bytes32 functionId_, bytes calldata callData_) external;
+
+    function timelockParametersOf(address contract_, bytes32 functionId_) external view returns (uint128 delay, uint128 duration);
 
     function unscheduleCall(address caller_, bytes32 functionId_, bytes calldata callData_) external;
 
