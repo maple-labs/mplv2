147194fea1df28dde8f49148886fd395c27ab4c6
JG Carvalho <jg@jgcarv.com>
2023-06-28 11:18:01 -0300

    feat: add integration test for migrator

--- tests/utils/CompilerResolver.sol
+++ tests/utils/CompilerResolver.sol
@@ -3,3 +3,5 @@ pragma solidity 0.8.7;
 
 // This file is needed to force a compile of MapleGlobals, so the artifact is available to deploy using the foundry cheatcode.
 import { MapleGlobals } from "../../modules/globals/contracts/MapleGlobals.sol";
+import { Migrator }     from "../../modules/migrator/contracts/Migrator.sol";
+import { MockERC20 }    from "../../modules/erc20/contracts/test/mocks/MockERC20.sol";
