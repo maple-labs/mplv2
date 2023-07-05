201a284ea8b65adf6fb35c39ca03ba6e996daa35
JG Carvalho <jg@jgcarv.com>
2023-07-05 11:33:53 -0300

    fix: PR comments

--- tests/integration/InflationModule.t.sol
+++ tests/integration/InflationModule.t.sol
@@ -9,8 +9,8 @@ import { MapleToken }            from "../../contracts/MapleToken.sol";
 import { MapleTokenInitializer } from "../../contracts/MapleTokenInitializer.sol";
 import { MapleTokenProxy }       from "../../contracts/MapleTokenProxy.sol";
 
-import { TestBase }       from "../utils/TestBase.sol";
-import { IGlobalsLike  }  from "../utils/Interfaces.sol";
+import { TestBase }      from "../utils/TestBase.sol";
+import { IGlobalsLike  } from "../utils/Interfaces.sol";
 
 contract InflationModuleIntegrationTest is TestBase {
 
@@ -30,8 +30,8 @@ contract InflationModuleIntegrationTest is TestBase {
         vm.prank(governor);
         globals.setMapleTreasury(treasury);
 
-        token   = IMapleToken(address(new MapleTokenProxy(address(globals), address(new MapleToken()), address(new MapleTokenInitializer()), migrator)));
-        module  = new InflationModule(address(globals), address(token));
+        token  = IMapleToken(address(new MapleTokenProxy(address(globals), address(new MapleToken()), address(new MapleTokenInitializer()), migrator)));
+        module = new InflationModule(address(globals), address(token));
 
         vm.startPrank(governor);        
         globals.scheduleCall(address(token), "MT:ADD_MODULE", abi.encodeWithSelector(IMapleToken.addModule.selector, module, false, true));
@@ -62,20 +62,20 @@ contract InflationModuleIntegrationTest is TestBase {
 
         vm.expectRevert("MT:M:NOT_MINTER");
         module.claim();
-
-        vm.stopPrank();
     }
 
     function test_inflationModule_claim_success_sameWindow() external {
         uint256 startingBalance = token.balanceOf(treasury);
         uint256 supply          = token.totalSupply();
 
-        vm.warp(start + 1000);
+        vm.warp(start + 1000 seconds);
 
         module.claim();
 
         assertEq(token.balanceOf(treasury), startingBalance + 1_000e18);
         assertEq(token.totalSupply(),       supply + 1_000e18);
+
+        assertEq(module.lastClaimed(), start + 1000 seconds);
     }
 
     function test_inflationModule_claim_success_multiWindow() external {
@@ -94,7 +94,7 @@ contract InflationModuleIntegrationTest is TestBase {
         uint256 startingBalance = token.balanceOf(treasury);
         uint256 supply          = token.totalSupply();
 
-        vm.warp(start + 1000);
+        vm.warp(start + 1000 seconds);
 
         module.claim();
 
