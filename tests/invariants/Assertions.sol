0179574cf4fbdd66fd6d4290874d2cd03daaad2e
Vedran Bidin <vedran@bidin.me>
2023-07-10 15:33:06 +0300

    fix: update invariant definitions

--- tests/invariants/Assertions.sol
+++ tests/invariants/Assertions.sol
@@ -10,12 +10,8 @@ contract Assertions is TestBase {
     /**************************************************************************************************************************************/
 
     // TODO Are these invariants needed? Can we reuse existing ERC20 invariants?
-
     // Invariant A: ∑balanceOf(account) == totalSupply
-    function assert_mapleToken_invariant_A(address mapleToken) internal { }
-
     // etc.
-    function assert_mapleToken_invariant_B(address mapleToken) internal { }
 
     /**************************************************************************************************************************************/
     /*** Emergency Module Invariants                                                                                                    ***/
@@ -27,35 +23,41 @@ contract Assertions is TestBase {
     /*** Inflation Module Invariants                                                                                                    ***/
     /**************************************************************************************************************************************/
 
-    // Verifies the integrity of the linked list of windows by traversing it from the start and ensuring the last window can be reached:
+    // Verifies the integrity of the linked list of windows by traversing it from the start and ensuring the last window can be reached.
     // Invariant A (v1): traverseFrom(zeroWindowId) == lastScheduledWindowId
     // Invariant A (v2): traverseFrom(lastClaimedWindowId) == lastScheduledWindowId
-    function assert_inflationModule_invariant_A(address inflationModule) internal { }
 
-    // Verify the size of the linked list of windows is not off the charts.
-    // Invariant B (v1): countWindowsFrom(zeroWindowId) <= maximumWindows
-    // Invariant B (v1): countWindowsFrom(lastClaimedWindowId) <= maximumWindows
-    // Invariant B (v3): lastScheduledWindowId <= maximumWindows
-    function assert_inflationModule_invariant_B(address inflationModule) internal { }
+    // Verify the zero index window is the first window.
+    // Invariant B (v1): zeroWindow.windowStart == 0
+    // Invariant B (v2): zeroWindow.windowStart == block.timestamp
+
+    // Verify the zero index window is not issuing any tokens.
+    // Invariant C: zeroWindow.issuanceRate == 0
+
+    // Verify the size of the linked list of windows is within limits.
+    // Invariant D (v1): countWindowsFrom(zeroWindowId) <= maximumWindows
+    // Invariant D (v1): countWindowsFrom(lastClaimedWindowId) <= maximumWindows
+    // Invariant D (v3): lastScheduledWindowId <= maximumWindows
 
     // Verify the last scheduled window is the last one in the linked list.
-    // Invariant C: lastScheduledWindow.nextWindowId == 0
-    function assert_inflationModule_invariant_C(address inflationModule) internal { }
+    // Invariant E: lastScheduledWindow.nextWindowId == 0
 
-    // Verify all windows are ordered in strictly ascending order:
-    // Invariant D: ∑window(window.windowStart < nextWindow.windowStart)
-    function assert_inflationModule_invariant_D(address inflationModule) internal { }
+    // Verify all windows are ordered in strictly ascending order by ids:
+    // Invariant F: ∑window(window.windowId < window.nextWindowId)
+
+    // Verify all windows are ordered in strictly ascending order by timestamps:
+    // Invariant G: ∑window(window.windowStart < nextWindow.windowStart)
 
     // Verify all window issuance rates are lower than the maximum issuance rate.
-    // Invariant E: ∑window(window.issuanceRate <= maximumIssuanceRate)
-    function assert_inflationModule_invariant_E(address inflationModule) internal { }
+    // Invariant H: ∑window(window.issuanceRate <= maximumIssuanceRate)
 
-    // Verify future tokens can not be claimed.
-    // Invariant F: block.timestamp >= lastClaimed
-    function assert_inflationModule_invariant_F(address inflationModule) internal { }
+    // Verify tokens can only be claimed up to the current time.
+    // Invariant I: lastClaimedTimestamp <= block.timestamp
+
+    // Verify the window of the last claim is set correctly based on the timestamp of the last claim.
+    // Invariant J: windowOf(lastClaimedTimestamp) == lastClaimedWindowId
 
     // Verify the calculation of how many tokens are claimable at the current time.
-    // Invariant G: claimable(block.timestamp) == manual calculation
-    function assert_inflationModule_invariant_Gs(address inflationModule) internal { }
+    // Invariant K: claimable(block.timestamp) == manual calculation
 
 }
