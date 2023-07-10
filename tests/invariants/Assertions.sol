de6765cfe49116ffcc676335effaab4d6ca1eb95
Vedran Bidin <vedran@bidin.me>
2023-07-10 18:37:31 +0300

    feat: Define `InflationModule` invariants (#23)
    * feat: define im invariants
    
    * fix: address audit comments
    
    * fix: refactor im
    
    * fix: add additional assertions
    
    * fix: remove console
    
    * fix: adjust invariants
    
    * fix: update invariant definitions
    
    * fix: update invariant comments

---
+++ tests/invariants/Assertions.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.18;
+
+import { TestBase } from "../utils/TestBase.sol";
+
+contract Assertions is TestBase {
+
+    /**************************************************************************************************************************************/
+    /*** Maple Token Invariants                                                                                                         ***/
+    /**************************************************************************************************************************************/
+
+    // TODO Are these invariants needed? Can we reuse existing ERC20 invariants?
+    // Invariant A: ∑balanceOf(account) == totalSupply
+    // etc.
+
+    /**************************************************************************************************************************************/
+    /*** Emergency Module Invariants                                                                                                    ***/
+    /**************************************************************************************************************************************/
+
+    // TODO: Is there any state to assert here?
+
+    /**************************************************************************************************************************************/
+    /*** Inflation Module Invariants                                                                                                    ***/
+    /**************************************************************************************************************************************/
+
+    // Verifies the integrity of the linked list of windows by traversing it from the start and ensuring the last window can be reached.
+    // Invariant A: traverseFrom(zeroWindowId) == lastScheduledWindowId
+
+    // Verify the last claimed window is contained in the linked list.
+    // Invariant Z: windows.contains(lastClaimedWindow)
+
+    // Verify the zero index window is the first window.
+    // Invariant B: zeroWindow.windowStart == 0
+
+    // Verify the zero index window is not issuing any tokens.
+    // Invariant C: zeroWindow.issuanceRate == 0
+
+    // TODO: Discuss more.
+    // Verify the size of the linked list of windows is within limits.
+    // Invariant D (v1): countWindowsFrom(zeroWindowId) <= maximumWindows
+    // Invariant D (v2): countWindowsFrom(lastClaimedWindowId) <= maximumWindows
+    // Invariant D (v3): lastScheduledWindowId <= maximumWindows
+
+    // Verify the last scheduled window is the last one in the linked list.
+    // Invariant E: lastScheduledWindow.nextWindowId == 0
+
+    // Verify all windows are ordered in strictly ascending order by ids:
+    // Invariant F: ∑window(window.windowId < window.nextWindowId)
+
+    // Verify all windows are ordered in strictly ascending order by timestamps:
+    // Invariant G: ∑window(window.windowStart < nextWindow.windowStart)
+
+    // TODO: Should MAX_IR be defined at the level of the contract?
+    // Verify all window issuance rates are lower than the maximum issuance rate.
+    // Invariant H: ∑window(window.issuanceRate <= maximumIssuanceRate)
+
+    // Verify tokens can only be claimed up to the current time.
+    // Invariant I: lastClaimedTimestamp <= block.timestamp
+
+    // Verify the window of the last claim is set correctly based on the timestamp of the last claim.
+    // Invariant J: windowOf(lastClaimedTimestamp) == lastClaimedWindowId
+
+    // Verify the calculation of how many tokens are claimable at the current time.
+    // Invariant K: claimable(block.timestamp) == manual calculation
+
+}
