1e9f65a16762e375b3dee32f66559103e3836cd2
0xfarhaan <59924029+0xfarhaan@users.noreply.github.com>
2023-07-21 11:49:06 +0100

    chore: add extra line

--- contracts/interfaces/IMapleTokenInitializer.sol
+++ contracts/interfaces/IMapleTokenInitializer.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.18;
 import { IERC20 } from "../../modules/erc20/contracts/interfaces/IERC20.sol";
 
 interface IMapleTokenInitializer is IERC20 {
+
     /**
      *  @dev    Contract was initialized
      *  @param  tokenMigrator Address of the Maple token migrator contract.
